dd752e17c4d0ea469295d451f782a870364bf64f
CyrusVII <137442376+CyrusVII@users.noreply.github.com>
2025-03-31 15:41:46 +0200

    3 parte hard

--- corsoPython/lunedi31.03/esercizioCompleto.py
+++ corsoPython/lunedi31.03/esercizioCompleto.py
@@ -37,61 +37,73 @@ def contInt(allNumber):
     return
 
 
-#inizio main
+# inizio main
 try:  
-  
-  #var per il while
-  b = True
-  while b:
-    #scelta dell utente 
-    st = int(input("-----\nCosa vuoi fare:\n1) Vediamo se il numero e pari \n2) Conto alla rovescia \n3) Calcoliamo il quadrato \n4) Gioco lista\n0) Abbandona \n---> "))
-    #match per la scelta
-    match st:
-      
-      case 0:#case di uscita
-        print("Arrivederci")
-        break
-      
-      case 1:#primo esercizio troviamo un numero positivo
-        n = int(input("Inserisci un numero -->"))
-        print(numEven(n))
-        
-      case 2:#contiamo alla rovescia
-        n = int(input("Inserisci un numero positivo-->"))
-        if(n > 0):
-          for i in range(n, -1, -1):
-            print(i)
-          else:
-            print("numero non positivo ripetere l operazione....")
+    # var per il while
+    b = True
+    while b:
+        taskNumber = int(input("Quante task vuoi fare: "))
+        while taskNumber > 0:
+            # scelta dell'utente 
+            st = int(input("-----\nCosa vuoi fare:\n1) Vediamo se il numero è pari \n2) Conto alla rovescia \n3) Calcoliamo il quadrato \n4) Gioco lista\n0) Abbandona \n---> "))
             
-      case 3:#lista di numeri
-        #creiamo una lista vuota e prendiamo l input per la grandezza della lista
-        allNumber = []
-        nList = int(input("Quanti numeri vuoi inserire? "))
-        #iteriamo per riempire la lista
-        listInsert(nList,allNumber)
-        #stampiamo il quadrato
-        for s in allNumber:
-          print(f"Il numero nella lista e {s} ---> {pow(s,2)} il quadrato.")
-          
-      case 4:#lista dell utente
-        #creiamo una lista vuota e prendiamo l input per la grandezza della lista
-        allNumber = []
-        nList = int(input("Quanti numeri vuoi inserire? "))
-        #iteriamo per riempire la lista
-        listInsert(nList,allNumber)
-        #controlliamo se la lista e piena
-        if len(allNumber) > 0:
-          #stampiamo il numero massimo
-          print(f"Il numero massimo della lista e: {max(allNumber)} e ci sono {len(allNumber)} elementi nella lista")
-          #controlliamo quanti valori ci sono
-          contInt(allNumber)
-        else:
-          print("La lista e vuota")
-    #domanda per ritornare al menu
-    b = True if input("Vuoir tornare al menu? s/n ---> ").lower().strip() == "s" else False
-  print("Fine programma")
+            # match per la scelta
+            match st:
+                case 0:  # case di uscita
+                    print("Arrivederci")
+                    break
+                
+                case 1:  # primo esercizio troviamo un numero positivo
+                    n = int(input("Inserisci un numero --> "))
+                    print(numEven(n))
+                
+                case 2:  # contiamo alla rovescia
+                    n = int(input("Inserisci un numero positivo --> "))
+                    if n > 0:
+                        for i in range(n, -1, -1):
+                            print(i)
+                    else:
+                        print("Numero non positivo, ripetere l'operazione...")
+                
+                case 3:  # lista di numeri
+                    # creiamo una lista vuota e prendiamo l'input per la grandezza della lista
+                    allNumber = []
+                    nList = int(input("Quanti numeri vuoi inserire? "))
+                    # iteriamo per riempire la lista
+                    listInsert(nList, allNumber)
+                    # stampiamo il quadrato
+                    for s in allNumber:
+                        print(f"Il numero nella lista è {s} ---> {pow(s,2)} il quadrato.")
+                
+                case 4:  # lista dell'utente
+                    # creiamo una lista vuota e prendiamo l'input per la grandezza della lista
+                    allNumber = []
+                    nList = int(input("Quanti numeri vuoi inserire? "))
+                    # iteriamo per riempire la lista
+                    listInsert(nList, allNumber)
+                    # controlliamo se la lista è piena
+                    if len(allNumber) > 0:
+                        # stampiamo il numero massimo
+                        print(f"Il numero massimo della lista è: {max(allNumber)} e ci sono {len(allNumber)} elementi nella lista")
+                        # controlliamo quanti valori ci sono
+                        contInt(allNumber)
+                    else:
+                        print("La lista è vuota")
+            
+            # decremento taskNumber per evitare loop infinito
+            taskNumber -= 1
+            #chiediamo all utente se vuole continuare con le task prescelte
+            if input("----\n Vuoi proseguire? s/n ---> ").lower().strip() == "s":
+              print("continua....")
+            else: 
+              break
+        # domanda per ritornare al menu
+        b = True if input("Vuoi tornare al menu? s/n ---> ").lower().strip() == "s" else False
+    
+    print("Fine programma")
+
 except ValueError:
-  print("Per favore, inserisci un numero valido!")
+    print("Per favore, inserisci un numero valido!")
+
   
 
