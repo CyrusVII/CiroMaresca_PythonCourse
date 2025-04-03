c0df36fa57a0f49027f70e16f11054dbb13c12e2
CyrusVII <137442376+CyrusVII@users.noreply.github.com>
2025-04-03 10:07:45 +0200

    creazione file classi

---
+++ corsoPython/giovedi.03.04/primeClassi.py
@@ -0,0 +1,9 @@
+# Le classi sono un'astrazione dei concetti del mondo reale.
+# • In Python, una classe è un modello per la creazione di oggetti.
+# • Un oggetto è un'istanza di una classe, cioè una copia univoca della classe che ha le sue proprietà uniche.
+# • Le classi sono definite usando la parola chiave class, seguita dal nome della classe.
+# Le classi possono contenere metodi e attributi.
+# • Gli attributi sono variabili associate a una classe.
+# • Gli attributi rappresentano le proprietà di un oggetto.
+# • Gli attributi di classe sono condivisi tra tutte le istanze della classe.
+# i metdoi sono funzione associate a una classe che rappresentano il comportamento di un ogetto
