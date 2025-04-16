651a99d4b47ddc4d86c8c8d6df9a25d055b05a3e
CyrusVII <137442376+CyrusVII@users.noreply.github.com>
2025-04-16 17:47:33 +0200

    last

---
+++ corsoPython/week14.04-28.04/mercoledi16.04/prnoNumPy.py
@@ -0,0 +1,12 @@
+#studia da slide 221 -225
+#pip install numpy
+
+import numpy as np
+#creazione di un array unidimensionale
+arr = np.array([1,2,3,4,5])
+
+#creazione di un array bidimensionale
+arr2d = np.array([[1,2,3],[4,5,6]])
+
+print(arr)
+print(arr2d)
